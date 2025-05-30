c8932a4811d7a9cb6f9116bec0d273aa1ae8a7e1
Nickteca <isaaclunaavila@gmail.com>
2025-05-30 01:30:12 -0600

    sfsfsfs

--- src/main/java/com/www/elReyDelPollo/component/JwtFilter.java
+++ src/main/java/com/www/elReyDelPollo/component/JwtFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -40,7 +41,7 @@ public class JwtFilter extends OncePerRequestFilter {
             var auth = new UsernamePasswordAuthenticationToken(
                     username,             // principal
                     null,                 // credencial (ya no importa)
-                    List.of()             // aquí podrías cargar roles si los tuvieras
+                    List.of(new SimpleGrantedAuthority("ROLE_USER"))             // aquí podrías cargar roles si los tuvieras
             );
             SecurityContextHolder.getContext().setAuthentication(auth);
 
